d9d2fdbded8b1fa5f8010f660fade906b775bbdd
ntvu <ntvu111@gmail.com>
2019-06-12 17:11:48 +0700

    Update AliceMaid - HomePage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var content=$(".tagProject"),tabs=$("span");
+    var content=$(".ProjectImg"),tabs=$("span");
     
     tabs.on('click', function(){
   
@@ -8,7 +8,6 @@ $(document).ready(function(){
       
       content.isotope({
         filter: filter
-  
       });
       return false;
     });
@@ -20,10 +19,64 @@ $(document).ready(function(){
   $(document).ready(function(){
     $('.CommentCaousel').slick({
       dots: true,
+      nextArrow: false,
+      prevArrow: false
     });
   });
 
+/////event for hamburger
+
+$(document).ready(function(){
+	$('.hamburger,slice').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+$(document).ready(function(){
+	$('.hamburger').click(function(){
+		$('.MoblieNav').slideToggle(300);
+	});
+});
+//////serarch button
+$(document).ready(function(){
+	$('.SearchDesk').click(function(){
+		$('.bagContainer input').slideToggle(300);
+	});
+});
 
   // ExpertTag Carousel
-  
+  $('.CarouselStaff').slick({
+    nextArrow: '<i class="ion-arrow-right-c"></i>',
+    prevArrow: '<i class="ion-arrow-left-c"></i>',
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
   
